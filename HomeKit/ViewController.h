8fe0115a667a523ec72782485c19f19df317a79a
kangkang <930115220@qq.com>
2017-12-27 15:17:43 +0800

    Initial Commit

---
+++ HomeKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeKit
+//
+//  Created by zving on 2017/12/27.
+//  Copyright © 2017年 metal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
